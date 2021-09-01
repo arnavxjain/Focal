f81cce3b83db3e44b29f7617409af8a2b5f7e119
arnavjainn06 <jainarnav@outlook.com>
2021-09-01 13:54:03 +0530

    time running system

--- page/main.js
+++ page/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const noRes = document.getElementsByClassName("nores")[0];
 const moreLink = document.getElementsByClassName("more")[0];
+const moreLink2 = document.getElementsByClassName("more")[1];
 const items = document.getElementsByClassName("items")[0];
 const moreItems = document.getElementsByClassName("more-items-page")[0];
 const moreItemsMore = document.getElementsByClassName("more-items")[0];
@@ -21,10 +22,22 @@ let scriptDomains;
 let mainHTML;
 const back = document.getElementsByClassName("fa-chevron-left")[0];
 
-
-// chrome.storage.sync.set( { domains: [] } );
+const timer = document.getElementsByClassName("timer")[0];
 
 const input = document.getElementById("new-res");
+
+chrome.storage.sync.get('dist', (dist) => {
+    console.log(dist.dist);
+    if (dist.dist < 0) {
+        timer.style.display = "none";
+    } else {
+        console.log('not less than 0');
+        chrome.storage.sync.get('timer', (timers) => {
+            runTimer(timers);
+            console.log(timers.timer);
+        });
+    } 
+});
 
 chrome.storage.sync.get('domains', (data) => {
     console.log('domains', data.domains);
@@ -73,6 +86,19 @@ moreLink.addEventListener("click", () => {
     }
 });
 
+moreLink2.addEventListener("click", () => {
+    moreItems.style.display = "flex";
+
+    for (let x = 0; x < scriptDomains.length; x++) {
+        moreItemsMore.innerHTML += `
+        <div class="restricted-item">
+            <span>${scriptDomains[x]}</span>
+            <i class="fas fa-trash"></i>
+        </div>
+        `;
+    }
+});
+
 back.addEventListener("click", () => {
     moreItems.style.animation = "slidein 0.3s"
     setTimeout(() => {
@@ -89,7 +115,9 @@ input.addEventListener("keypress", (e) => {
         } else {
             console.log("pushing!", e.target.value);
             scriptDomains.push(e.target.value);
+
             chrome.storage.sync.set( { domains: scriptDomains } );
+            
             if (scriptDomains.length < 3) {
                 items.innerHTML += 
             `
@@ -108,4 +136,56 @@ input.addEventListener("keypress", (e) => {
             }
         }
     }
+});
+
+// const hour = document.getElementById("hour");
+// const minutes = document.getElementById("minutes");
+// const seconds = document.getElementById("seconds");
+const timeLarge = document.getElementsByClassName("time-large")[0];
+
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+const runTimer = (time) => {
+    let date = new Date();
+    let month = months[date.getMonth()];
+    let dateDay = date.getDate();
+    let year = date.getFullYear();
+
+    const statement = `${month} ${dateDay}, ${year} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const statement2 = `${month} ${dateDay}, ${year} ${date.getHours()}:${date.getMinutes() + time}:${date.getSeconds()}`;
+    
+    var countDownDate = new Date(statement2).getTime();
+
+    timer.style.display = "flex";
+
+    setInterval(() => {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+
+        var hour2 = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes2 = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds2 = Math.floor((distance % (1000 * 60)) / 1000);
+
+        console.log(hour2, minutes2, seconds2);
+        // hour.textContent = `${hour2}:`;
+        // minutes.textContent = `${minutes2}:`;
+        // seconds.textContent = seconds2;
+        timeLarge.textContent = `${hour2}:${minutes2}:${seconds2}`;
+        chrome.storage.sync.set( { timer: `${hour2}:${minutes2}:${seconds2}` } );
+        chrome.storage.sync.set( { dist: distance } );
+
+        if (distance < 0) {
+            clearInterval(runTimer);
+            timer.style.display = "none";
+        }
+    }, 1000);
+}
+
+const startBtn = document.getElementById("start-btn");
+const timeValue = document.getElementById("time-value");
+let time;
+
+startBtn.addEventListener("click", () => {
+    time = parseInt(timeValue.value);
+    runTimer(time);
 });
